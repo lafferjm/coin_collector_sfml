73a472c162af141d7adbf5d350f7bc35a876381e
Justin Lafferty <justin.lafferty@kw.com>
2025-07-28 19:49:30 -0400

    draw window with green background

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
+#include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 
 const int WIDTH = 1024;
 const int HEIGHT = 768;
 
 int main(int argc, char **argv) {
-  sf::Window window(sf::VideoMode({WIDTH, HEIGHT}), "Coin Collector");
+  sf::RenderWindow window(sf::VideoMode({WIDTH, HEIGHT}), "Coin Collector");
+  window.setFramerateLimit(60);
 
   while (window.isOpen()) {
     while (const std::optional event = window.pollEvent()) {
@@ -14,6 +16,10 @@ int main(int argc, char **argv) {
         window.close();
       }
     }
+
+    window.clear(sf::Color::Green);
+
+    window.display();
   }
   return 0;
 }
