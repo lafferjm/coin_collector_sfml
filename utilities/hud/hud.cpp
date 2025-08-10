0b5b5601404cf83a6677d202acb71ae7f0b73960
Justin Lafferty <justin.lafferty@kw.com>
2025-08-09 20:30:53 -0400

    update font rendering

--- utilities/hud/hud.cpp
+++ utilities/hud/hud.cpp
@@ -1,14 +1,16 @@
 #include "hud.hpp"
 #include <string>
 
-HUD::HUD() : m_font("../assets/fonts/arcade_classic.ttf"), m_score_text(m_font) {
-    m_score_text.setString("Score: 0");
-    m_score_text.setCharacterSize(24);
-    m_score_text.setPosition(sf::Vector2f(25.f, 10.f));
+HUD::HUD() : m_font("../assets/fonts/press_start.ttf"), m_score_text(m_font) {
+    m_score_text.setString("Score:0");
+    m_score_text.setCharacterSize(45);
+    m_score_text.setPosition(sf::Vector2f(15.f, 10.f));
+    m_score_text.setFillColor(sf::Color(0xee, 0x00, 0xef));
+
 }
 
 void HUD::set_score(int score) {
-    m_score_text.setString("Score: " + std::to_string(score));
+    m_score_text.setString("Score:" + std::to_string(score));
 }
 
 void HUD::draw(sf::RenderWindow &target) const {
